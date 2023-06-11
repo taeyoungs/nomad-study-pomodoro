fb2ce0d42dfb01acfeae5022e08a0182080f7087
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-06-11 16:45:58 +0900

    mod: App 컴포넌트 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
-function App() {
-  return <div>App</div>;
-}
+import styled from 'styled-components';
 
-export default App;
+import { Timer, Score } from 'components';
+
+import { color } from 'constants/common';
+
+const Layout = styled.div`
+  background-color: ${color.bg};
+  min-height: 100vh;
+  height: 100%;
+  padding: 30px 0;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  color: ${color.white};
+  font-size: 36px;
+  font-weight: 700;
+  margin: 0 0 60px;
+`;
+
+export default function App() {
+  return (
+    <Layout>
+      <Title>Pomodoro</Title>
+      <Timer />
+      <Score />
+    </Layout>
+  );
+}
